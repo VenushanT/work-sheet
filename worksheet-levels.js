0c4c5407a180a54b04d2e0392d4b102099e6a946
VenushanT <aron28416@gmail.com>
2026-04-21 10:24:30 +0530

    Initial worksheet setup

---
+++ worksheet-levels.js
@@ -0,0 +1,8 @@
+// Edit this file to control English Level labels shown in the worksheet header.
+// Example header output: English Level: 2A8
+window.WORKSHEET_LEVELS_CONFIG = {
+  sectionLevels: {
+    '3A3I': '3A3I',
+    '2A8': '2A8'
+  }
+};
